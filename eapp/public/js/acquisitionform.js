986429941363f2804d6c282a4e2a4730c810996d
Smruti Padhy <smruti.padhy@gmail.com>
2017-04-24 16:12:40 -0400

    code clean up, dependencies added

--- eapp/public/js/acquisitionform.js
+++ eapp/public/js/acquisitionform.js
@@ -19,7 +19,6 @@ $.ajax({
           if(tforms[i] != ".DS_Store"){
             $("#tforms").append('<option value="'+ tforms[i]+'">'+ tforms[i] +'</option>')
           }
-          //count++
       }
     }
   }
@@ -33,9 +32,8 @@ $("#tforms").change(function(){
 
 function addAqFields(formName){
   //check if form is in local storage
-  //let termform = JSON.parse(localStorage.getItem('termform'))
   let termform = JSON.parse(localStorage.getItem(formName))
-  console.log("selectedFields",termform)
+  console.log("Form Selected:",termform)
 
   let url = "http://localhost:3000/acquisitions/forms/" + formName
 
@@ -52,15 +50,19 @@ function addAqFields(formName){
         add_term_to_form(termform)
       }//data
     })
+
   } else{
     add_term_to_form(termform)
   }
 
 }//end of addAqFields function
 
+/**
+Add fields to the acquistion form UI using a specified JSON file
+*/
 function add_term_to_form(termform){
   selectedFields = termform['fields']
-  console.log(selectedFields.length)
+  console.log("Number of Fields in the form: ",selectedFields.length)
   //console.log("x",x.length)
 
   for (let i=0; i<selectedFields.length; i++){
@@ -68,6 +70,8 @@ function add_term_to_form(termform){
     let options = []
     let sub_options1 = []
     let nvalues = []
+
+    // check the 'notes' field for any value specified
     let notes = checkNotes(selectedFields[i].name,selectedFields[i].notes)
     if(notes != null){
       nvalues = Object.keys(notes).map(function(key) {
@@ -75,139 +79,133 @@ function add_term_to_form(termform){
           })
     }
 
-      if(selectedFields[i].valueRange == null){
-        /* Case1: No Value Range */
-        $("#ndar-fields").append('<div class="form-group">\
+    if(selectedFields[i].valueRange == null){
+      /* Case1: No Value Range */
+      $("#ndar-fields").append('<div class="form-group">\
         <label for="ndar-'+i+'" data-toggle="tooltip" title="'+selectedFields[i].name+'">'+selectedFields[i].description+'</label>\
         <div>\
         <input class="form-control" type="text" placeholder="'+selectedFields[i].valueRange+'" id="ndar-'+i+'">\
         </div>\
         </div>')
-      }else if (selectedFields[i].valueRange.indexOf(';')> -1){
-        /*  Case 2:
-        if valueRange specified with ';' separator
-            check notes if values with its meaning specified in the notes
-            if notes is empty then parse valueRange field
-            otherwise parse notes field and obtain values representation, e.g (1 = "xyz"; 2 = "utty")
-         */
-         let sub_options2 = []
-          options = selectedFields[i].valueRange.split(';')
+    }else if (selectedFields[i].valueRange.indexOf(';')> -1){
+      /*  Case 2:
+      if valueRange specified with ';' separator
+      check notes if values with its meaning specified in the notes
+      if notes is empty then parse valueRange field
+      otherwise parse notes field and obtain values representation, e.g (1 = "xyz"; 2 = "utty")
+      */
+      let sub_options2 = []
+      options = selectedFields[i].valueRange.split(';')
           //if(notes== {}){
           //  options = selectedFields[i].valueRange.split(';')
           //} else
           //if((notes != null) && (Object.values(notes).length ==  options.length)){
           //  options = Object.values(notes)
           //}
-          console.log("c2::options::", options)
-          console.log("c2::options.length::", options.length)
-          $("#ndar-fields").append('<div class="form-group">\
-            <label for="ndar-'+i+'" data-toggle="tooltip" title="'+ selectedFields[i].name+'">'+selectedFields[i].description+'</label>\
+      console.log("c2::options::", options)
+      console.log("c2::options.length::", options.length)
+      $("#ndar-fields").append('<div class="form-group">\
+        <label for="ndar-'+i+'" data-toggle="tooltip" title="'+ selectedFields[i].name+'">'+selectedFields[i].description+'</label>\
+        <div>\
+          <select class="form-control" id="ndar-'+i+'">\
+            <option value="nsource">Select</option>\
+            </select>\
+          </div>\
+        </div>')
+
+      for (let j=0; j< options.length; j++){
+        console.log("Adding:",options[j])
+        if(options[j].indexOf("::")> -1){
+          let sub_options = options[j].split("::")
+          for(let k=sub_options[0];k<=sub_options[1];k++){
+            //$("#"+sid).append('<option value="'+ k+'">'+ k +'</option>')
+            sub_options2.push(k)
+          }
+        }else{
+          sub_options2.push(options[j])
+          //$("#"+sid).append('<option value="'+ options[j]+'">'+ options[j] +'</option>')
+        }
+      }
+      if((notes != null) && (nvalues.length ==  sub_options2.length)){
+        //options = Object.values(notes)
+        options = nvalues
+        for(let m=0;m<options.length;m++){
+          $("#"+sid).append('<option value="'+ options[m]+'">'+ options[m] +'</option>')
+        }
+      }else{
+        for(let m=0;m<sub_options2.length;m++){
+          $("#"+sid).append('<option value="'+ sub_options2[m]+'">'+ sub_options2[m] +'</option>')
+        }
+      }
+    } else if (selectedFields[i].valueRange.indexOf("::")> -1){
+      /*
+      * Case3: valueRange of the form - 0::3
+      * check notes - parse notes
+      */
+      flag = false
+      if(notes == {}){
+        sub_options1 = selectedFields[i].valueRange.trim().split("::")
+      } else{
+        //sub_options1 = Object.values(notes)
+        sub_options1 = nvalues
+        console.log("c3::sub_options1:: ", sub_options1)
+        console.log("c3::sub_options1.length:: ", sub_options1.length)
+        //console.log("notes: ", notes)
+        if(sub_options1.length == 1){
+          sub_options1 = selectedFields[i].valueRange.trim().split("::")
+        }
+        //console.log(":: ",sub_options1)
+      }
+
+      console.log("c3-1::sub-options1:: ",sub_options1)
+      console.log("c3-1::sub_options1.length:: ", sub_options1.length)
+
+      if(sub_options1[1].trim()>20){
+        $("#ndar-fields").append('<div class="form-group">\
+          <label for="ndar-'+i+'" data-toggle="tooltip" title="'+selectedFields[i].name+'">'+selectedFields[i].description+'</label>\
             <div>\
-              <select class="form-control" id="ndar-'+i+'">\
-              <option value="nsource">Select</option>\
-              </select>\
+              <input class="form-control" type="text" placeholder="'+selectedFields[i].valueRange+'" id="ndar-'+i+'">\
             </div>\
           </div>')
 
-          //console.log(sid)
-          for (let j=0; j< options.length; j++){
-            console.log("Adding",options[j])
-            if(options[j].indexOf("::")> -1){
-              let sub_options = options[j].split("::")
-              for(let k=sub_options[0];k<=sub_options[1];k++){
-                //$("#"+sid).append('<option value="'+ k+'">'+ k +'</option>')
-                sub_options2.push(k)
-              }
-            }else{
-              sub_options2.push(options[j])
-              //$("#"+sid).append('<option value="'+ options[j]+'">'+ options[j] +'</option>')
-            }
+      }else{
+        $("#ndar-fields").append('<div class="form-group">\
+          <label for="ndar-'+i+'" data-toggle="tooltip" title="'+ selectedFields[i].name+'">'+selectedFields[i].description+'</label>\
+          <div>\
+            <select class="form-control" id="ndar-'+i+'">\
+              <option value="select">Select</option>\
+            </select>\
+          </div>\
+          </div>')
+
+        if(notes == null || notes.hasOwnProperty(selectedFields[i].name)){
+          for(let m=sub_options1[0].trim();m<sub_options1[1].trim();m++){
+            $("#"+sid).append('<option value="'+ m+'">'+ m +'</option>')
           }
-          //if((notes != null) && (Object.values(notes).length ==  sub_options2.length)){
-          if((notes != null) && (nvalues.length ==  sub_options2.length)){
-            //options = Object.values(notes)
-            options = nvalues
-            for(let m=0;m<options.length;m++){
-              $("#"+sid).append('<option value="'+ options[m]+'">'+ options[m] +'</option>')
-            }
-          }else{
-            for(let m=0;m<sub_options2.length;m++){
-              $("#"+sid).append('<option value="'+ sub_options2[m]+'">'+ sub_options2[m] +'</option>')
-            }
+        }else{
+          for(let m=0;m<sub_options1.length;m++){
+            $("#"+sid).append('<option value="'+ sub_options1[m]+'">'+ sub_options1[m] +'</option>')
           }
+        }
+      }
+    }else{
 
+      $("#ndar-fields").append('<div class="form-group">\
+      <label for="ndar-'+i+'" data-toggle="tooltip" title="'+selectedFields[i].name+'">'+selectedFields[i].description+'</label>\
+        <div>\
+          <input class="form-control" type="text" placeholder="'+selectedFields[i].valueRange+'" id="ndar-'+i+'">\
+        </div>\
+      </div>')
+    }
 
-        } else if (selectedFields[i].valueRange.indexOf("::")> -1){
-          /*
-          Case3: valueRange of the form - 0::3
-          check notes - parse notes
+  }//end of outermost for
 
-          */
-          flag = false
-          if(notes == {}){
-            sub_options1 = selectedFields[i].valueRange.trim().split("::")
-
-          } else{
-            //sub_options1 = Object.values(notes)
-            sub_options1 = nvalues
-            console.log("c3::sub_options1:: ", sub_options1)
-            console.log("c3::sub_options1.length:: ", sub_options1.length)
-            //console.log("notes: ", notes)
-            if(sub_options1.length == 1){
-              sub_options1 = selectedFields[i].valueRange.trim().split("::")
-            }
-            //console.log(":: ",sub_options1)
-          }
-
-            console.log("c3-1::sub-options1:: ",sub_options1)
-            console.log("c3-1::sub_options1.length:: ", sub_options1.length)
-
-            if(sub_options1[1].trim()>20){
-              $("#ndar-fields").append('<div class="form-group">\
-              <label for="ndar-'+i+'" data-toggle="tooltip" title="'+selectedFields[i].name+'">'+selectedFields[i].description+'</label>\
-              <div>\
-              <input class="form-control" type="text" placeholder="'+selectedFields[i].valueRange+'" id="ndar-'+i+'">\
-              </div>\
-              </div>')
-            }else{
-            $("#ndar-fields").append('<div class="form-group">\
-              <label for="ndar-'+i+'" data-toggle="tooltip" title="'+ selectedFields[i].name+'">'+selectedFields[i].description+'</label>\
-              <div>\
-                <select class="form-control" id="ndar-'+i+'">\
-                <option value="select">Select</option>\
-                </select>\
-              </div>\
-            </div>')
-
-            if(notes == null || notes.hasOwnProperty(selectedFields[i].name)){
-              for(let m=sub_options1[0].trim();m<sub_options1[1].trim();m++){
-                $("#"+sid).append('<option value="'+ m+'">'+ m +'</option>')
-              }
-            }else{
-              for(let m=0;m<sub_options1.length;m++){
-                $("#"+sid).append('<option value="'+ sub_options1[m]+'">'+ sub_options1[m] +'</option>')
-              }
-            }
-          }
-          }
-          else{
-            //$("#"+sid).append('<option value="'+ options[j]+'">'+ options[j] +'</option>')
-            $("#ndar-fields").append('<div class="form-group">\
-            <label for="ndar-'+i+'" data-toggle="tooltip" title="'+selectedFields[i].name+'">'+selectedFields[i].description+'</label>\
-            <div>\
-            <input class="form-control" type="text" placeholder="'+selectedFields[i].valueRange+'" id="ndar-'+i+'">\
-            </div>\
-            </div>')
-          }
-
-    }//end of outermost for
-
-    $("#ndar-fields").append('<div class="form-group">\
-    <label for="ndar-'+selectedFields.length+'" data-toggle="tooltip" title="ExperimentID">ExperimentID</label>\
-    <div>\
+  $("#ndar-fields").append('<div class="form-group">\
+  <label for="ndar-'+selectedFields.length+'" data-toggle="tooltip" title="ExperimentID">ExperimentID</label>\
+  <div>\
     <input class="form-control" type="text" placeholder="ExperimentID" id="ndar-'+selectedFields.length+'">\
-    </div>\
-    </div>')
+  </div>\
+  </div>')
 }
 
 function saveAqInfo(e){
@@ -275,28 +273,4 @@ function checkNotes(key,notes){
   } else {
     return {}
   }
-}
-
-
-function addModal(mbody){
-  <!-- Modal -->
-$("#modal").append('<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">\
-  <div class="modal-dialog" role="document">\
-    <div class="modal-content">\
-      <div class="modal-header">\
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>\
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">\
-          <span aria-hidden="true">&times;</span>\
-        </button>\
-      </div>\
-      <div class="modal-body">\
-        '+ mbody +'\
-      </div>\
-      <div class="modal-footer">\
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>\
-        <button type="button" class="btn btn-primary">Save changes</button>\
-      </div>\
-    </div>\
-  </div>\
-</div>')
 }
