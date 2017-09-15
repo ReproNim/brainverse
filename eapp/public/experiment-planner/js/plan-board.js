3f26eb75b3d15266cfa43d413a51cfb91f7e7f08
Smruti Padhy <smruti.padhy@gmail.com>
2017-09-14 20:12:29 -0400

    Edit Plan info implemented

--- eapp/public/experiment-planner/js/plan-board.js
+++ eapp/public/experiment-planner/js/plan-board.js
@@ -7,7 +7,26 @@ var resArray = []
 
 console.log("newPlanObj: ", newPlanObj)
 
-$('#planInfo').append('<h4>'+ newPlanObj['Name']+' <a href="#"><span class="fa fa-pencil" style="float:right;"></span></a></h4><hr>')
+$('#planInfo').append('<h4 id="pname">'+ newPlanObj['Name']+' <a data-toggle="modal" href="#updatePlanInfoModal"><span class="fa fa-pencil" style="float:right;"></span></a></h4><hr>')
+$('#planInfo').append(createModal('updatePlanInfoModal', 'Update Plan Info', 'Update'))
+let expInfoForm = new AlpacaForm('#body-updatePlanInfoModal')
+createPlanInfoForm(expInfoForm,"updatePlanInfoModal", newPlanObj["Name"],newPlanObj["Description"])
+
+$(document).on('hidden.bs.modal','#updatePlanInfoModal', function(e){
+  console.log("plan name",  $("#updatePlanInfoModal-name").val())
+  console.log("desc: ", $("#planDescription").val())
+  let name = $("#updatePlanInfoModal-name").val()
+  let desc = $("#planDescription").val()
+  if(name != ""){
+    newPlanObj["Name"] = $("#updatePlanInfoModal-name").val()
+  }
+  if(desc!= ""){
+    newPlanObj["Description"] = $("#planDescription").val()
+  }
+  localStorage.setItem("newPlanObj", JSON.stringify(newPlanObj))
+  $('#pname').html(newPlanObj['Name']+' <a data-toggle="modal" href="#updatePlanInfoModal"><span class="fa fa-pencil" style="float:right;"></span></a>')
+})
+
 $('#div-addColumn').append(addSessionColumn())
 $(document).on('hidden.bs.modal','#newSessionModal', function(e){
   let sname = $('#sessionName').val()
@@ -25,6 +44,13 @@ $(document).on('hidden.bs.modal','#newSessionModal', function(e){
   $('#div-addColumn').append(addSessionColumn())
   $('#div-planBoard').append('<div class="col-md-7" id="div-kanban"></div>')
   createKanbanBoard(sname,sname)
+  $('#div-kanban').on('columnAttrClicked', function(event){
+    var args = event.args;
+    if (args.attribute == "button") {
+      console.log("args.attribute:", args)
+      args.cancelToggle = true;
+    }
+  })
 })
 
 function createKanbanBoard(name,label){
@@ -33,16 +59,16 @@ function createKanbanBoard(name,label){
   kCO["resources"] = new $.jqx.dataAdapter(setResources())
   kCO["source"] = new $.jqx.dataAdapter(setSources(name,label))
   kCO["itemRenderer"] = function(element, item, resource)
-        {
-            $(element).find(".jqx-kanban-item-color-status").html("<span style='line-height: 23px; margin-left: 5px; color:white;'>" + resource.name + "</span>");
-        }
+    {
+      $(element).find(".jqx-kanban-item-color-status").html("<span style='line-height: 23px; margin-left: 5px; color:white;'>" + resource.name + "</span>");
+    }
   kCO["columns"]= columnArray
   kCO["columnRenderer"] = function (element, collapsedElement, column) {
     var columnItems = $("#div-kanban").jqxKanban('getColumnItems', column.dataField).length;
     // update header's status.
     element.find(".jqx-kanban-column-header-status").html(" (" + columnItems + ")");
-    element.find("div.jqx-window-collapse-button-background.jqx-kanban-column-header-custom-button").after('<div class="jqx-window-collapse-button-background jqx-kanban-column-header-custom-button"><div style="width: 100%; height: 100%; left:-30px; top:-15px" class="fa-edit-icon"></div></div>')
-    console.log("column.HeaderElement: ", column.headerElement)
+    element.find("div.jqx-window-collapse-button-background.jqx-kanban-column-header-custom-button").after('<div class="jqx-window-collapse-button-background jqx-kanban-column-header-custom-button"><a data-toggle="modal" href="#updateSessionModal"><div style="width: 100%; height: 100%; left:-30px; top:-15px" class="fa-edit-icon"></div></a></div>')
+    $('#updateSessionModal').append(updateSessionColumn)
     console.log("element:-->",element)
     // update collapsed header's status.
     collapsedElement.find(".jqx-kanban-column-header-status").html(" (" + columnItems +  ")");
--- eapp/public/experiment-planner/js/plan-utils.js
+++ eapp/public/experiment-planner/js/plan-utils.js
@@ -29,6 +29,71 @@ function addSessionColumn(){
   </div>'
   return htmlStr
 }
+function updateSessionColumn(){
+  let htmlStr = '\
+        <div id="updateSessionModal" class="modal fade" role="dialog">\
+        <div class="modal-dialog">\
+        <!-- Modal content-->\
+          <div class="modal-content">\
+          <div class="modal-header">\
+            <button type="button" class="close" data-dismiss="modal">&times;</button>\
+            <h4 class="modal-title" style="text-align-last: center">Update Session</h4>\
+          </div>\
+          <div class="modal-body">\
+            <form>\
+              <div class="form-group">\
+                <label for="sessionName">Session Name</label>\
+                <input type="text" class="form-control" id="sessionName" placeholder="Session Name">\
+              </div>\
+            </form>\
+          </div>\
+          <div class="modal-footer" style="text-align:center">\
+            <button type="button" class="btn btn-default" data-dismiss="modal">Update</button>\
+          </div>\
+        </div>\
+      </div>\
+    </div>'
+  return htmlStr
+}
+function createModal(modalID, title,modalFooter){
+  let htmlStr = '\
+        <div id="'+modalID+'" class="modal fade" role="dialog">\
+        <div class="modal-dialog">\
+        <!-- Modal content-->\
+          <div class="modal-content">\
+          <div class="modal-header">\
+            <button type="button" class="close" data-dismiss="modal">&times;</button>\
+            <h4 class="modal-title" style="text-align-last: center">'+title+'</h4>\
+          </div>\
+          <div class="modal-body"><div id=body-'+modalID+'></div></div>\
+          <div class="modal-footer" style="text-align:center">\
+            <button type="button" class="btn btn-default" data-dismiss="modal" id="btn-close-'+ modalID+'">'+modalFooter+'</button>\
+          </div>\
+        </div>\
+      </div>\
+    </div>'
+  return htmlStr
+}
+function createForm(form, modalID){
+  form.inputForm('Name', 'Name', modalID-'name', 'string', undefined, null, false)
+  form.alpacaGen()
+}
+function createPlanInfoForm(form, modalID, name, desc){
+  form.inputForm('Name', 'Name', modalID+'-name', 'string', false, name, false)
+  form.textAreaForm('Description', 'Description', 'planDescription','string', undefined, desc, false)
+  //form.submitBtnForm('Update Plan',sAction)
+  form.alpacaGen()
+}
+function sAction(){
+  console.log("Action performed")
+  planObj["Name"] = $("#planName").val()
+  planObj["Description"] = $("#planDescription").val()
+  console.log("planObj: ", planObj)
+  localStorage.setItem("newPlanObj", JSON.stringify(planObj))
+  form.alpacaDestroy()
+  window.location.href = serverURL+"/html/plan-board.html"
+}
+
 var getIconClassName = function () {
   return "jqx-icon-plus-alt"
   //return "fa-edit-icon"
@@ -89,15 +154,6 @@ function setResources(){
 }
 
 function setTemplate(){
-  /*var template = "<div class='jqx-kanban-item' id=''>"
-    + "<div class='jqx-kanban-item-color-status'></div>"
-    + "<div style='display: none;' class='jqx-kanban-item-avatar'></div>"
-    + "<div class='jqx-icon jqx-icon-close jqx-kanban-item-template-content jqx-kanban-template-icon'></div>"
-    + "<div class='jqx-kanban-item-text'></div>"
-    + "<div class='jqx-kanban-item-content'></div>"
-    + "<div style='display: none;' class='jqx-kanban-item-footer'></div>"
-    + "</div>"*/
-
   var template = "<div class='jqx-kanban-item' id=''>"
                 + "<div class='jqx-kanban-item-color-status'></div>"
                 + "<div style='display: none;' class='jqx-kanban-item-avatar'></div>"
