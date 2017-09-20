461457cc55ca9a0e5a2af8a408181249ff9422ff
Smruti Padhy <smruti.padhy@gmail.com>
2017-09-20 16:22:43 -0400

    fixed and improvised directory creation with Promise

--- eapp/config/ref-config.js
+++ eapp/config/ref-config.js
@@ -4,3 +4,12 @@
 //However, this file is being tracked
 exports.clientId = ''
 exports.clientSecret = ''
+exports.dirPaths = [
+  "/../uploads/",
+  "/../uploads/dataDictionary",
+  "/../uploads/termforms",
+  "/../uploads/acquisition",
+  "/../uploads/acquisition/experiments",
+  "/../uploads/acquisition/plans",
+  "/../uploads/plansdocs"
+]
