1769c1b09aa7ecfe2d1f1e4d7b69af979e5e9f70
Smruti Padhy <smruti.padhy@gmail.com>
2017-12-29 17:31:23 -0500

    addded summary of plan execution

---
+++ eapp/modules/data-collection/js/dc-form-2.js
@@ -0,0 +1,101 @@
+collectionObj = JSON.parse(localStorage.getItem("collectionObj"))
+console.log("[dc-form-2] collectionObj: ", collectionObj)
+planObjSelected = JSON.parse(localStorage.getItem('planObjSelected'))
+console.log("[dc-form-2] planObjSelected: ", planObjSelected)
+let actionObj = JSON.parse(localStorage.getItem('action'))
+console.log("[dc-form-2] actionObj: ", actionObj)
+
+dataTableSource = JSON.parse(localStorage.getItem('dataTableSource'))
+
+$('#projectId').append('<h5> Project Name: '+ collectionObj['Name'] +'</h5>')
+$('#subjectId').append('<h5> Subject ID: '+ actionObj['subjectId'] +'</h5>')
+$('#planId').append('<h5> Plan: '+ planObjSelected['Project Name'] +'</h5>')
+
+loadPlan(planObjSelected)
+
+function loadPlan(plan){
+  var source = {
+    localData: dataTableSource,
+    dataType: "array",
+    dataFields: [{
+        name: 'sessionNumber',
+        type: 'string'
+    }, {
+        name: 'sessionName',
+        type: 'string'
+    }, {
+        name: 'taskName',
+        type: 'string'
+    }, {
+        name: 'instrumentName',
+        type: 'string'
+    }, {
+        name: 'status',
+        type: 'string'
+    }]
+  }
+
+  var dataAdapter = new $.jqx.dataAdapter(source)
+  
+  $("#activityTable").jqxDataTable({
+      width: 1000,
+      theme: 'energyblue',
+      //pageable: true,
+      //pagerMode: 'advanced',
+      filterable: true,
+      source: dataAdapter,
+      columns: [{
+          text: 'Session Number',
+          dataField: 'sessionNumber',
+          width: 120
+      }, {
+          text: 'Session Name',
+          dataField: 'sessionName',
+          width: 200
+      }, {
+          text: 'Task Name',
+          editable: false,
+          dataField: 'taskName',
+          width: 280
+      }, {
+          text: 'Instrument Name',
+          dataField: 'instrumentName',
+          width: 300
+          //cellsAlign: 'right',
+          //align: 'right'
+      }, {
+          text: 'Status',
+          dataField: 'status',
+          width: 100
+          //cellsAlign: 'right',
+          //align: 'right',
+          //cellsFormat: 'c2'
+      }]
+  })
+  $('#activityTable').on('rowClick', function (event) {
+    var args = event.args
+    var row = args.row
+    var index = args.index;
+    // row key
+    var rowKey = args.key;
+
+    console.log("args: ", args)
+    console.log("row clicked:", row)
+    console.log("index: ", index)
+    console.log("rowKey: ", rowKey)
+    event.stopPropagation()
+    row['subjectId'] = actionObj['subjectId']
+
+    localStorage.setItem("action",JSON.stringify(row))
+    window.location.href = serverURL+"/data-collection/html/dc-form.html"
+
+
+    /*$("#activityTable").jqxDataTable('updateRow', index, {
+        sessionNumber: row.sessionNumber,
+        sessionName: row.sessionName,
+        taskName: row.taskName,
+        instrumentName:row.instrumentName,
+        status: "completed"
+    });*/
+
+  })
