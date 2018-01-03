734ed988b3a7cd7ecf161a985490e786fe66029b
Smruti Padhy <smruti.padhy@gmail.com>
2018-01-02 22:40:37 -0500

    Methods to add activity node to the graph added

--- eapp/util/nidme-graph.js
+++ eapp/util/nidme-graph.js
@@ -19,14 +19,18 @@ var _rdfStoreSetup = function(){
   rstore.rdf.setPrefix("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
   rstore.rdf.setPrefix("nda","https://ndar.nih.gov/api/datadictionary/v2/dataelement/")
   rstore.rdf.setPrefix("prov","http://www.w3.org/ns/prov#")
-  rstore.setPrefix("xsd", "http://www.w3.org/2001/XMLSchema#")
-  rstore.setPrefix("dc", "http://purl.org/dc/terms/")
+  rstore.rdf.setPrefix("xsd", "http://www.w3.org/2001/XMLSchema#")
+  rstore.rdf.setPrefix("dc", "http://purl.org/dc/terms/")
+  rstore.rdf.setPrefix("provone", "http://purl.org/provone#")
+
   _addToStoreNamespace("nidm", "http://purl.org/nidash/nidm#")
   _addToStoreNamespace("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
   _addToStoreNamespace("nda","https://ndar.nih.gov/api/datadictionary/v2/dataelement/")
   _addToStoreNamespace("prov","http://www.w3.org/ns/prov#")
   _addToStoreNamespace("xsd","http://www.w3.org/2001/XMLSchema#")
   _addToStoreNamespace("dc","http://purl.org/dc/terms/")
+  _addToStoreNamespace("provone","http://purl.org/provone#")
+
   console.log("Namespaces:---->", namespaces)
   //let cpath = path.join(__dirname, '/../../../uploads/acquisition/')
   console.log("userData path:-- ", userData)
@@ -40,8 +44,14 @@ function loadFilesToRDFStore(path){
   var listOfFiles = new Promise(function(resolve){
     fs.readdir(path, function(err,list){
       if(err) throw err;
-      console.log("lists: ", list)
-      resolve(list)
+      let filesList = []
+      for(let i=0;i< list.length;i++){
+        if(list[i]!==".DS_Store"){
+          filesList.push(list[i])
+        }
+      }
+      console.log("loadFilesToRDFStore :lists: ", filesList)
+      resolve(filesList)
     })
   })
   listOfFiles.then(function(list){
@@ -51,6 +61,7 @@ function loadFilesToRDFStore(path){
         let name = f.split(".")
         return new Promise(function(resolve){
           store.load('text/turtle',data,"nidm:"+name[0], function(err,results){
+            console.log("name[0]: ", name[0], "   err:", err)
             resolve(name[0])
           })
         })
@@ -250,10 +261,135 @@ var NIDMGraph = class NIDMGraph {
     }
     return ndaNode
   }
+
+  /**
+  ** Add Project Activity Node to the graph
+  **/
+  addProjectActivity(jsonObj){
+    let projObj = jsonObj['Project']
+    let projectId = "nidm:activity_" + projObj['ID']
+    let projectNode = store.rdf.createNamedNode(store.rdf.resolve(projectId))
+    this.rgraph.add(store.rdf.createTriple(projectNode,
+    store.rdf.createNamedNode(store.rdf.resolve("rdf:type")),
+    store.rdf.createNamedNode(store.rdf.resolve("prov:Activity"))))
+    for(var key1 in projObj){
+      let key = key1.replace(/\s+/g, '')
+      if(key === 'created'){
+        this.rgraph.add(store.rdf.createTriple(projectNode,
+        store.rdf.createNamedNode(store.rdf.resolve("dc:created")),
+        store.rdf.createLiteral(projObj["created"],null,store.rdf.resolve("xsd:dateTime"))))
+      }else if(key === 'wasDerivedFrom'){
+        this.rgraph.add(store.rdf.createTriple(projectNode,
+        store.rdf.createNamedNode(store.rdf.resolve("prov:wasDerivedFrom")),
+        store.rdf.createNamedNode(store.rdf.resolve("nidm:activity_"+projObj['wasDerivedFrom']))))
+      } else{
+        this.rgraph.add(store.rdf.createTriple(projectNode,
+        store.rdf.createNamedNode(store.rdf.resolve("nidm:"+key)),
+        store.rdf.createLiteral(projObj[key1])))
+      }
+    }
+    if(jsonObj.hasOwnProperty('PlanID')){
+      this.rgraph.add(store.rdf.createTriple(projectNode,
+      store.rdf.createNamedNode(store.rdf.resolve("prov:hadPlan")),
+      store.rdf.createNamedNode(store.rdf.resolve("nidm:entity_"+jsonObj['PlanID']))))
+    }
+    return projectNode
+  }
+  addSessionActivity(jsonObj, projectNode){
+    let sessionObj = jsonObj['Session']
+    let sessionId = "nidm:activity_" + sessionObj['SessionID']
+    let sessionNode = store.rdf.createNamedNode(store.rdf.resolve(sessionId))
+    this.rgraph.add(store.rdf.createTriple(sessionNode,
+    store.rdf.createNamedNode(store.rdf.resolve("rdf:type")),
+    store.rdf.createNamedNode(store.rdf.resolve("prov:Activity"))))
+    for(var key1 in sessionObj){
+      let key = key1.replace(/\s+/g, '')
+      this.rgraph.add(store.rdf.createTriple(sessionNode,
+        store.rdf.createNamedNode(store.rdf.resolve("nidm:"+key)),
+        store.rdf.createLiteral(sessionObj[key1])))
+    }
+    this.rgraph.add(store.rdf.createTriple(sessionNode,
+    store.rdf.createNamedNode(store.rdf.resolve("provone:isPartOf")),
+    projectNode))
+    return sessionNode
+  }
+
+  addAcquisitionActivity(jsonObj, sessionNode){
+    let acqActivityObj = jsonObj['AcquisitionActivity']
+    let taskId = "nidm:activity_" + acqActivityObj['AcquisitionActivityID']
+    let taskNode = store.rdf.createNamedNode(store.rdf.resolve(taskId))
+    this.rgraph.add(store.rdf.createTriple(taskNode,
+    store.rdf.createNamedNode(store.rdf.resolve("rdf:type")),
+    store.rdf.createNamedNode(store.rdf.resolve("prov:Activity"))))
+    for(var key1 in acqActivityObj){
+      let key = key1.replace(/\s+/g, '')
+      this.rgraph.add(store.rdf.createTriple(taskNode,
+        store.rdf.createNamedNode(store.rdf.resolve("nidm:"+key)),
+        store.rdf.createLiteral(acqActivityObj[key1])))
+    }
+    this.rgraph.add(store.rdf.createTriple(taskNode,
+    store.rdf.createNamedNode(store.rdf.resolve("provone:isPartOf")),
+    sessionNode))
+
+    this.rgraph.add(store.rdf.createTriple(taskNode,
+    store.rdf.createNamedNode(store.rdf.resolve("prov:used")),
+    store.rdf.createNamedNode(store.rdf.resolve("nidm:"+jsonObj['InstrumentName']))))
+    return taskNode
+  }
+
+  //TODO: Add Role
+  addAgent(jsonObj, taskNode,sessionNode){
+    let agentId = "nidm:agent_" + jsonObj['SubjectID']
+    let agentNode = store.rdf.createNamedNode(store.rdf.resolve(agentId))
+    this.rgraph.add(store.rdf.createTriple(agentNode,
+    store.rdf.createNamedNode(store.rdf.resolve("rdf:a")),
+    store.rdf.createNamedNode(store.rdf.resolve("prov:Agent"))))
+
+    this.rgraph.add(store.rdf.createTriple(agentNode,
+    store.rdf.createNamedNode(store.rdf.resolve("prov:wasAssociatedWith")),
+    taskNode))
+
+    this.rgraph.add(store.rdf.createTriple(agentNode,
+    store.rdf.createNamedNode(store.rdf.resolve("prov:wasAssociatedWith")),
+    sessionNode))
+    return agentNode
+  }
+
+  addAcquisitionObject(jsonObj, taskNode, agentNode){
+    let acqId = "nidm:entity_" + jsonObj['objID']
+    let acqFieldObj = jsonObj['fields']
+    let acqNode = store.rdf.createNamedNode(store.rdf.resolve(acqId))
+    this.rgraph.add(store.rdf.createTriple(acqNode,
+    store.rdf.createNamedNode(store.rdf.resolve("rdf:type")),
+    store.rdf.createNamedNode(store.rdf.resolve("prov:Entity"))))
+    for(var key1 in acqFieldObj){
+      let key = key1.replace(/\s+/g, '')
+      if(key === 'interview_date'){
+        console.log("key: ", key, " acqFieldOb:", acqFieldObj[key])
+        this.rgraph.add(store.rdf.createTriple(acqNode,
+        store.rdf.createNamedNode(store.rdf.resolve("nidm:interview_date")),
+        store.rdf.createLiteral(acqFieldObj[key],null,store.rdf.resolve("xsd:dateTime"))))
+      }else{
+        this.rgraph.add(store.rdf.createTriple(acqNode,
+          store.rdf.createNamedNode(store.rdf.resolve("nidm:"+key)),
+          store.rdf.createLiteral(acqFieldObj[key1])))
+      }
+    }
+    this.rgraph.add(store.rdf.createTriple(acqNode,
+    store.rdf.createNamedNode(store.rdf.resolve("prov:wasGeneratedBy")),
+    taskNode))
+
+    this.rgraph.add(store.rdf.createTriple(acqNode,
+    store.rdf.createNamedNode(store.rdf.resolve("prov:wasAttributedTo")),
+    agentNode))
+    return acqNode
+  }
+
+
 } // End of Class definition
 
 /**
-** Add/Insert the NIDM graph created to the store with sepecifc URI
+** Add/Insert the NIDM graph created to the store with specifc URI
 **/
 function _addToStore(nidmGraph,graphId,addCallback){
   store.insert(nidmGraph.rgraph, graphId, function(err) {
@@ -368,6 +504,7 @@ var _saveToRDFstore = function(nidmGraph, graphId, fileName,callback_tstring){
       tstring = tstring + "@prefix prov: <"+ store.rdf.prefixes.get("prov")+"> .\n"
       tstring = tstring + "@prefix dc: <"+ store.rdf.prefixes.get("dc")+"> .\n"
       tstring = tstring + "@prefix xsd: <"+ store.rdf.prefixes.get("xsd")+"> .\n"
+      tstring = tstring + "@prefix provone: <"+ store.rdf.prefixes.get("provone")+"> .\n"
     } else{
       console.log('Some other error: ', err.code);
     }
