a10991c86a8903df6711a2fbf0aec6e076605d25
Sanu Ann Abraham <abraham.sanuann@gmail.com>
2018-04-10 10:16:41 -0400

    NF+RF - added instrument editor back button and refactored corresponding html file - Closes#142

--- eapp/config/app-config.js
+++ eapp/config/app-config.js
@@ -1,5 +1,5 @@
-exports.clientId = ''
-exports.clientSecret = ''
+exports.clientId = 'f41a0704070ceb1e1064'
+exports.clientSecret = '8553ed076a4103a5f8b0f4eec6fdd29efadb3f84'
 exports.dirPaths = [
   "/uploads/",
   "/uploads/dataDictionary",
